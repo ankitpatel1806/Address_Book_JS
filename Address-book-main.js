8ee0ed7008a74717fbe0e99db054f81f7f8559ce
Ankit Patel <yugap1806@gmail.com>
2025-03-05 04:55:39 +0530

    Ankit patel added uc3_add_new_address_book

--- Address-book-main.js
+++ Address-book-main.js
@@ -7,6 +7,10 @@ try {
         "Ankit", "Patel", "Bhopal", "India", "In", "100001", "9876543210", "yugap1806@gmail.com"
     );
     console.log(addressBook.addContact(contact1));
+    const contact2 = new Contact(
+        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
+    )
+    console.log(addressBook.addContact(contact2)); 
     console.log(addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
